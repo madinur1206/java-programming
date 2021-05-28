fc9ca144b4701c750acdc25749f3282f42a98c91
madinur mahmud <madinur1206@gmail.com>
2021-05-27 22:09:39 -0400

    added  2 more new classes

---
+++ src/day47_constructors/studentObjects.java
@@ -0,0 +1,12 @@
+package day47_constructors;
+
+public class studentObjects {
+    public static void main(String[] args) {
+        student st1 = new student ();
+        student st2 = new student ();
+        student st3 = new student ("Burak");
+        student st4 = new student ("Alihan");
+        student st5 = new student (35);
+        student st6 = new student ("madinur",18);
+    }
+}
